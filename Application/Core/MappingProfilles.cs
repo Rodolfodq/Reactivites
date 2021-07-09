931c985b6e35c52c8eef7d4731db7eb13793c500
RODOLFO QUEIROZ <rodolfo.dqueiroz@gmail.com>
2021-07-09 10:29:23 -0300

    End of Section 4

---
+++ Application/Core/MappingProfilles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain;
+
+namespace Application.Core
+{
+    public class MappingProfilles : Profile
+    {
+         public MappingProfilles()
+        {
+            CreateMap<Activity, Activity>();
+        }
+    }
+}
